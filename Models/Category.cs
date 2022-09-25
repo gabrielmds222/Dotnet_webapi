98fb5895540ddf59d430e22c0f83ff12d1222472
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-09-25 11:22:13 -0300

    feat: first project

---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace apiDotnetOne.Models{
+    public class Category{
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string Title { get; set; }
+    }
+}
